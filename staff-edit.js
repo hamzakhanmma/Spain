eb5dbd5ab302196fa8c828e854078fe5d18e8502
Hamza Khan <hamzakhan74x@gmail.com>
2026-02-02 12:37:36 +0000

    Update staff-edit.js

--- staff-edit.js
+++ staff-edit.js
@@ -1,12 +1,1 @@
-db.collection("registrations").add({
-  name,
-  studentId,
-  email,
-  timestamp: new Date()
-});
 
-db.collection("registrations").get().then(snapshot => {
-  snapshot.forEach(doc => {
-    // show student + delete button
-  });
-});
